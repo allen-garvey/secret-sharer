1944a96362898eeb48e7b939d0118033ca1efca6
Allen Garvey <allengarvey@hotmail.com>
2023-08-08 16:30:46 -0400

    Add jsdoc to items store

--- models/items-store.js
+++ models/items-store.js
@@ -4,8 +4,18 @@ const THREE_HOURS = 1000 * 60 * 60 * 3;
 
 const store = new Map();
 
+/**
+ * @typedef {Object} Item
+ * @property {string} title
+ * @property {string} content
+ */
+
 const getIdGenerator = (isProd) => isProd ? uuidv4 : () => Math.ceil(Math.random() * 1000).toString();
 
+/**
+ * @return {string}
+ * @param {Item} item
+ */
 const setItem = (item) => {
     const idGenerator = getIdGenerator(process.env.NODE_ENV === 'production');
     let id;
@@ -22,6 +32,10 @@ const setItem = (item) => {
     return id;
 };
 
+/**
+ * @return {Item|undefined}
+ * @param {string} id
+ */
 const getItem = (id) => store.get(id);
 
 module.exports = {set: setItem, get: getItem};
