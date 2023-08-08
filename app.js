e2bf689d952f74028e4de1aea02171cea2922447
Allen Garvey <allengarvey@hotmail.com>
2023-08-08 16:16:25 -0400

    Remove users routes as they are not used

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ var path = require('path');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -18,7 +17,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
